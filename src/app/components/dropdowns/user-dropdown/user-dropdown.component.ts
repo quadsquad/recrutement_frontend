dead77640fc615f95bbb7861b9e9c71f6ee230a5
unknown <mohamedamine@esprit.tn>
2021-12-01 23:41:25 +0100

    updata admin dashboard

--- src/app/components/dropdowns/user-dropdown/user-dropdown.component.ts
+++ src/app/components/dropdowns/user-dropdown/user-dropdown.component.ts
@@ -1,21 +1,21 @@
-import { Component, AfterViewInit, ViewChild, ElementRef } from "@angular/core";
-import { createPopper } from "@popperjs/core";
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { createPopper } from '@popperjs/core';
 
 @Component({
-  selector: "app-user-dropdown",
-  templateUrl: "./user-dropdown.component.html",
+  selector: 'app-user-dropdown',
+  templateUrl: './user-dropdown.component.html',
 })
 export class UserDropdownComponent implements AfterViewInit {
   dropdownPopoverShow = false;
-  @ViewChild("btnDropdownRef", { static: false }) btnDropdownRef: ElementRef;
-  @ViewChild("popoverDropdownRef", { static: false })
+  @ViewChild('btnDropdownRef', { static: false }) btnDropdownRef: ElementRef;
+  @ViewChild('popoverDropdownRef', { static: false })
   popoverDropdownRef: ElementRef;
   ngAfterViewInit() {
     createPopper(
       this.btnDropdownRef.nativeElement,
       this.popoverDropdownRef.nativeElement,
       {
-        placement: "bottom-start",
+        placement: 'bottom-start',
       }
     );
   }
