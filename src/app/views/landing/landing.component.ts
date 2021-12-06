3b18dba998a2edffe3c97f23cd7393f4e91716a7
syrine868 <syrine868@gmail.com>
2021-12-06 15:10:30 +0100

    update auth

--- src/app/views/landing/landing.component.ts
+++ src/app/views/landing/landing.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import {JobServiceService} from '../../services/jobs/job-service.service';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
 })
 export class LandingComponent implements OnInit {
-  constructor() {}
+  constructor(private js: JobServiceService) {}
+  dataJobs:any =[];
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.js.getAllJobs().subscribe((res : [])=>{
+      this.dataJobs=res
+    }, (err)=>{
+      console.log(err);
+    });
+  }
 
 
 }
