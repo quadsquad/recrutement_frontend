19a97dde4e8041def15835d9a6f2cc4af99d5109
Sofien Ressaissi <sofienressaissi@gmail.com>
2021-12-26 20:06:04 +0100

    heroku deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
   production: true,
+  apiUrl: 'http://localhost:9090/',
+  jobUrlapi: 'https://jobrecruitement.herokuapp.com/',
+  authUrl : 'https://authrecruitement.herokuapp.com',
+  nodeUrl: 'https://node-recrutement.herokuapp.com',
+  phoneUrl: 'https://phonevalidation.abstractapi.com',
+  apiKeyPhone: '44571ae9a979475b90b68ec42224722b',
+  candidaturesUrl : 'http://localhost:8090/'
 };
